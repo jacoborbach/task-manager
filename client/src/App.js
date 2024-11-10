f2a1a24a98131f3000a4e9f050f9b8df9d32650b
jacoborbach <jacoborbach96@gmail.com>
2024-11-10 16:18:11 +0200

    added basic todo adding functionalityt

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import React, { useState } from 'react';
+
+
+const todos = [
+  {"id": "1", "todo": "something"}, 
+  {"id": "2", "todo": "somethingelse"}]
+
+function App() {
+
+  const [todo, setTodo] = useState('')
+
+  const addtodo = () => {
+    console.log(todo)
+    todos.push({"id": "3", "todo": todo})
+    console.log(todos)
+    setTodo('')
+  };
+  const editTodo = {};
+
+  
+  return (
+    <div className="App">
+      <h2 className="Header">Jake's Task List</h2>
+
+      <input 
+        type="text" 
+        placeholder="Add todo"
+        value={todo}
+        onChange={e => setTodo(e.target.value)}/>
+        <button onClick={addtodo}>Add Task</button>
+
+      <ul className="ToDos">
+    {todos.map((todo, i) => (
+      <li key={i}>
+        <span>
+          {todo.todo}
+        </span>    
+        {/* <button onClick={editTodo}>Edit</button>     */}
+      </li>
+    ))}
+    </ul>
+    </div>
+  );
+}
+
+export default App;
